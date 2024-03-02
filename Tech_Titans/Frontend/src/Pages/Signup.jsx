20f1a0777ab49599691baee59c89877d840c1fda
ayann07 <126085856+ayann07@users.noreply.github.com>
2024-03-02 14:04:03 +0530

    Login & Signup pages added

---
+++ Tech_Titans/Frontend/src/Pages/Signup.jsx
@@ -0,0 +1,47 @@
+
+import React, { useState } from 'react';
+import { Box, Typography, TextField, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const Signup = () => {
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({
+    name:'',
+    email: '',
+    phone:'',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setInputs(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+
+  return (
+    <div style={{ backgroundColor: 'inherit', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <form onSubmit={handleSubmit}>
+        <Box maxWidth={450} width="100%" display="flex" flexDirection="column" alignItems="center" justifyContent="center" padding={3} borderRadius={5} boxShadow={3} bgcolor="white">
+          <Typography variant='h4' textAlign="center" fontFamily="Times New Roman" color="black"  mb={3}>Register</Typography>
+          <TextField label='Name' value={inputs.name} onChange={handleChange} name='name' margin='normal' type='text' required />
+          <TextField label='Email' value={inputs.email} onChange={handleChange} name='email' margin='normal' type='email' required />
+          <TextField label='Phone Number' value={inputs.phone} onChange={handleChange} name='phone' margin='normal' type='text' required />
+          <TextField label='Password' value={inputs.password} onChange={handleChange} name='password' margin='normal' type='password' required />
+          <Button variant='contained' type='submit' color='success' sx={{ my: 2, width: '30%' }}>Submit</Button>
+          <Button sx={{color:'black'}} onClick={() => navigate("/")} fullWidth>Already registered? Please Login</Button>
+        </Box>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
+
+
