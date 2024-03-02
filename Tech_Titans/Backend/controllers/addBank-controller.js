672073ec4ea8795fd7f645a7606032cd04177a20
Aditya Gupta <adityagupta11219@gmail.com>
2024-03-02 16:02:45 +0530

    added accounts model & controller

---
+++ Tech_Titans/Backend/controllers/addBank-controller.js
@@ -0,0 +1,14 @@
+const Account = require("../models/account-model");
+
+const addBank = async (req, res) => {
+	const {username,account_no,bank_name,balance} = req.body;
+    let account = await Account.findOne({account_no});
+    if(!account){
+        account = await Account.create({username,bank_name,account_no,balance});
+        res.status(200).json({message:"Account added successfully",data:account});
+    }else{
+        res.status(403).json({message:"Account already exists"});
+    }
+};
+
+module.exports = {addBank}
--- Tech_Titans/Backend/routes/auth-router.js
+++ Tech_Titans/Backend/routes/auth-router.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const {home,login,register,logout,getuser}=require('../controllers/auth-controller');
+const {addBank}  = require('../controllers/addBank-controller')
 const isAuthenticatedUser=require('../Middleware/authMiddleware');
 
 router.route('/').get(home)
@@ -8,5 +9,6 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
 router.route('/user').get(isAuthenticatedUser,getuser);
+router.route('/addBank').post(isAuthenticatedUser,addBank);
 
 module.exports=router;
